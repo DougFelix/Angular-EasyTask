9514fc88150da08d1ba4b677dc99f7862bc73e36
DougFelix <dotfreitas@gmail.com>
2024-06-30 20:42:48 -0300

    Switching from standalone to Module and using standalone components together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,8 @@ import { TasksComponent } from './components/tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, UserComponent, TasksComponent],
 })
 export class AppComponent {
   users = DUMMY_USERS;
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent],
+})
+export class AppModule {}
